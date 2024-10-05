7029b256cc86fd722c77334565020ff0df828fb1
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-05 09:19:24 -0300

    Configurações de Projeto - ImperialFlix

--- projeto_Individual_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/configs/SwaggerConfig.java
+++ projeto_Individual_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/configs/SwaggerConfig.java
@@ -1,5 +1,51 @@
 package br.com.projeto.imperialflix.configs;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+@SecurityScheme(name = "bearer Auth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig {
+
+	@Value("${prop.swagger.dev-url}")
+	private String devUrl;
+
+	@Bean
+	public OpenAPI myOpenAPI() {
+		Server server = new Server();
+		server.setUrl(devUrl);
+		server.setDescription("Development Server");
+
+		Contact contact = new Contact();
+		contact.setEmail("jgmoreira1809@gmail.com");
+		contact.setName("João Gabriel Moreira");
+		contact.setUrl("joaomoreira.net");
+
+		License license = new License();
+		license.setName("Apache License 2.0");
+		license.setUrl("https://www.apache.org/licenses/LICENSE-2.0.html");
+
+		Info info = new Info();
+		info.setTitle("Pessoa & Série API");
+		info.setVersion("1.0.0");
+		info.setDescription("API to manage people and series.");
+		info.setContact(contact);
+		info.setLicense(license);
+		info.setTermsOfService("https://swagger.io/terms/");
+
+		return new OpenAPI().info(info).servers(List.of(server));
+
+	}
 
 }
--- projeto_Individual_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/controllers/FilmeController.java
+++ projeto_Individual_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/controllers/FilmeController.java
@@ -1,5 +1,14 @@
 package br.com.projeto.imperialflix.controllers;
 
-public class FilmeController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/Filme")
+public class FilmeController {
+	@GetMapping
+	public String olaMundo() {
+		return "Hello, world";
+	}
 }
