9ea4c50b7e5d0380e75fcefdefdea4acd337d4eb
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-12 18:15:47 -0300

    Revert "Organização de arquivos"
    This reverts commit 26bb64e07dfec83c180f15c8ceb860e5514522fe.

---
+++ projeto_Individual_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/ImperialflixApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.imperialflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperialflixApplication.class, args);
+	}
+
+}
