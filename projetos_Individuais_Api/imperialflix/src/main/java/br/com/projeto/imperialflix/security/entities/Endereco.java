53374b9530f4605fa682477d8b2f4b8a7c7d1578
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-16 15:09:14 -0300

    Migrando classe ENDERECO p/ SECURITY.ENTITIES

---
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/entities/Endereco.java
@@ -0,0 +1,131 @@
+package br.com.projeto.imperialflix.security.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+    private String regiao;
+
+    @OneToOne(mappedBy = "endereco", cascade = CascadeType.ALL)
+    private Cinema cinema;
+
+    public Endereco() {
+    }
+
+    public Endereco(Long id, String cep, String logradouro, String complemento, String bairro, String localidade, String uf, String estado, String regiao, Cinema cinema) {
+        this.id = id;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+        this.estado = estado;
+        this.regiao = regiao;
+        this.cinema = cinema;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
+
+    public Cinema getCinema() {
+        return cinema;
+    }
+
+    public void setCinema(Cinema cinema) {
+        this.cinema = cinema;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco [id=" + id + ", cep=" + cep + ", logradouro=" + logradouro + ", complemento=" + complemento
+                + ", bairro=" + bairro + ", localidade=" + localidade + ", uf=" + uf
+                + ", estado=" + estado + ", regiao=" + regiao + ", cinema=" + cinema + "]";
+    }
+}
