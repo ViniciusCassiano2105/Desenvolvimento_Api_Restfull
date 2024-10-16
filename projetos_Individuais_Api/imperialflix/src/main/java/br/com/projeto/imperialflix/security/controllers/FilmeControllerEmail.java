a44868ba7bb7264ed462cc6dec4d2fcf02b42621
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-16 15:12:21 -0300

    Migrando classe FILMECONTROLLEREMAIL p/ SECURITY.CONTROLLERS

--- projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/controllers/FilmeControllerEmail.java
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/controllers/FilmeControllerEmail.java
@@ -1,11 +1,12 @@
-package br.com.projeto.imperialflix.controllers;
+package br.com.projeto.imperialflix.security.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.projeto.imperialflix.services.EmailService;
+import br.com.projeto.imperialflix.security.services.EmailService;
+
 
 @RestController
 @RequestMapping("/Filme")
