fa8a1dad51d8b500a9fa3f89673e03a170b95669
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-16 15:02:41 -0300

    Migrando classe CINEMAREPOSITORY p/ SECURITY.REPOSITORIES

--- projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/repositories/CinemaRepository.java
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/repositories/CinemaRepository.java
@@ -1,9 +1,10 @@
-package br.com.projeto.imperialflix.repository;
+package br.com.projeto.imperialflix.security.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.projeto.imperialflix.entities.Cinema;
+import br.com.projeto.imperialflix.security.entities.Cinema;
+
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Integer>{
