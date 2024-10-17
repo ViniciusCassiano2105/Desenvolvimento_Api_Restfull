d83289e461046883bf4d7cc3aabfa89a6a1e06de
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-17 08:31:32 -0300

    teste-cep

--- projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/controllers/CinemaController.java
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/controllers/CinemaController.java
@@ -1,18 +1,32 @@
 package br.com.projeto.imperialflix.security.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import br.com.projeto.imperialflix.security.entities.Cinema;
-import br.com.projeto.imperialflix.security.services.CinemaService;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.imperialflix.security.dto.EnderecoRequestDTO;
+import br.com.projeto.imperialflix.security.dto.EnderecoResponseDTO;
+import br.com.projeto.imperialflix.security.entities.Cinema;
+import br.com.projeto.imperialflix.security.services.CinemaService;
+import br.com.projeto.imperialflix.security.services.EnderecoService;
 
 @RestController
 @RequestMapping("/cinemas")
 public class CinemaController {
 
+	@Autowired
+	EnderecoService enderecoService;
+	
     @Autowired
     private CinemaService cinemaService;
 
@@ -45,5 +59,16 @@ public class CinemaController {
     @GetMapping("/count")
     public long getCinemaCount() {
         return cinemaService.getCinemaCount();
+    }
+    
+	/*
+	 * @PostMapping("/teste-cep") public EnderecoResponseDTO
+	 * testeEndereco(@RequestParam String cep) { return
+	 * enderecoService.consultarEndereco(cep); }
+	 */
+    
+    @PostMapping("/teste-cep")
+    public EnderecoResponseDTO testeEndereco(@RequestBody EnderecoRequestDTO enderecoDTO) {
+    	return enderecoService.consultarEndereco(enderecoDTO);
     }
 }
