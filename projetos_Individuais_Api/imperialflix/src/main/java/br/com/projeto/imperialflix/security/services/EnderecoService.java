887df09b8e897d926574f729b842f6cf6a425777
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-17 08:22:23 -0300

    Implementação classe - EnderecoService

---
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/services/EnderecoService.java
@@ -0,0 +1,45 @@
+package br.com.projeto.imperialflix.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projeto.imperialflix.security.dto.EnderecoRequestDTO;
+import br.com.projeto.imperialflix.security.dto.EnderecoResponseDTO;
+import br.com.projeto.imperialflix.utils.Util;
+
+@Service
+public class EnderecoService {
+	@Autowired
+	Util util;
+	
+//	public EnderecoResponseDTO consultarEndereco(String cep) {
+	//	EnderecoResponseDTO viaCep = util.buscarEndereco(cep);
+		
+		//EnderecoResponseDTO endereco = new EnderecoResponseDTO();
+		//endereco.setCep(viaCep.getCep());
+		//endereco.setBairro(viaCep.getBairro());
+		//endereco.setComplemento(viaCep.getComplemento());
+		//endereco.setEstado(viaCep.getEstado());
+		//endereco.setLocalidade(viaCep.getLocalidade());
+		//endereco.setLogradouro(viaCep.getLogradouro());
+		//endereco.setRegiao(viaCep.getRegiao());
+		//endereco.setUf(viaCep.getUf());
+		//return endereco;
+	//}
+	
+	public EnderecoResponseDTO consultarEndereco(EnderecoRequestDTO enderecoDTO) {
+		EnderecoResponseDTO viaCep = util.buscarEndereco(enderecoDTO.getCep());
+		
+		EnderecoResponseDTO endereco = new EnderecoResponseDTO();
+		endereco.setCep(enderecoDTO.getCep());
+		endereco.setBairro(viaCep.getBairro());
+		endereco.setComplemento(enderecoDTO.getComplemento());
+		endereco.setEstado(viaCep.getEstado());
+		endereco.setLocalidade(viaCep.getLocalidade());
+		endereco.setLogradouro(viaCep.getLogradouro());
+		endereco.setRegiao(viaCep.getRegiao());
+		endereco.setUf(viaCep.getUf());
+		return endereco;
+	}
+}
+
