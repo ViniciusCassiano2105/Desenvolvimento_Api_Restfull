359b8536d1954209c20efd83aaf28c1d7cdeb68f
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-17 08:28:30 -0300

    Implementação classe EnderecoResponseDTO

---
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/dto/EnderecoResponseDTO.java
@@ -0,0 +1,78 @@
+package br.com.projeto.imperialflix.security.dto;
+
+public class EnderecoResponseDTO {
+
+	private String cep;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String uf;
+	private String estado;
+	private String regiao;
+	private String localidade;
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+
+}
