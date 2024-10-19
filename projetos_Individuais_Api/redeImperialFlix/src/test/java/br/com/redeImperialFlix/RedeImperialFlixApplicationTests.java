c3f03f2931ac2ed98089ad620219b90adeb23b86
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-19 14:56:30 -0300

    RedeImperialFlixApplicationTests

---
+++ projetos_Individuais_Api/redeImperialFlix/src/test/java/br/com/redeImperialFlix/RedeImperialFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.redeImperialFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeImperialFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
