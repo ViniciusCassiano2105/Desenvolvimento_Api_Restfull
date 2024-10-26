9378fea879fe7e479b337254c325e6acfc482c05
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-26 20:49:21 -0300

    Crud - CinemaController

--- projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/controllers/CinemaController.java
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/controllers/CinemaController.java
@@ -1,74 +1,64 @@
 package br.com.projeto.imperialflix.security.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
+import br.com.projeto.imperialflix.security.dto.CinemaRequestDTO;
+import br.com.projeto.imperialflix.security.dto.CinemaResponseDTO;
 import br.com.projeto.imperialflix.security.dto.EnderecoRequestDTO;
 import br.com.projeto.imperialflix.security.dto.EnderecoResponseDTO;
-import br.com.projeto.imperialflix.security.entities.Cinema;
 import br.com.projeto.imperialflix.security.services.CinemaService;
-import br.com.projeto.imperialflix.security.services.EnderecoService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
-@RequestMapping("/cinemas")
+@RequestMapping("/cinema")
 public class CinemaController {
-
-	@Autowired
-	EnderecoService enderecoService;
 	
     @Autowired
     private CinemaService cinemaService;
 
-    @GetMapping
-    public List<Cinema> getAllCinemas() {
-        return cinemaService.findAll();
-    }
-
+    // Buscar um cinema por ID
+    @SecurityRequirement(name = "Bearer Auth")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping("/{id}")
-    public Optional<Cinema> getCinemaById(@PathVariable Integer id) {
-        return cinemaService.findById(id);
+    public CinemaResponseDTO buscarCinema(@PathVariable Integer id) {
+        return cinemaService.buscarCinema(id);
     }
 
-    @PostMapping
-    public Cinema createCinema(@RequestBody Cinema cinema) {
-        return cinemaService.save(cinema);
-    }
-
-    @PutMapping("/{id}")
-    public Cinema updateCinema(@PathVariable Integer id, @RequestBody Cinema cinema) {
-        cinema.setId(id); 
-        return cinemaService.save(cinema);
-    }
-
+    // Deletar um cinema por ID
+    @SecurityRequirement(name = "Bearer Auth")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @DeleteMapping("/{id}")
-    public void deleteCinema(@PathVariable Integer id) {
-        cinemaService.deleteById(id);
+    public void delete(@PathVariable Integer id) {
+        cinemaService.deletarCinema(id);
     }
 
-    @GetMapping("/count")
-    public long getCinemaCount() {
-        return cinemaService.getCinemaCount();
+    // Criar um novo cinema
+    @SecurityRequirement(name = "Bearer Auth")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @PostMapping
+    public CinemaResponseDTO criarCinema(@RequestBody CinemaRequestDTO cinemaRequestDTO) {
+        return cinemaService.salvarCinema(cinemaRequestDTO);
     }
-    
-	/*
-	 * @PostMapping("/teste-cep") public EnderecoResponseDTO
-	 * testeEndereco(@RequestParam String cep) { return
-	 * enderecoService.consultarEndereco(cep); }
-	 */
-    
-    @PostMapping("/teste-cep")
-    public EnderecoResponseDTO testeEndereco(@RequestBody EnderecoRequestDTO enderecoDTO) {
-    	return enderecoService.consultarEndereco(enderecoDTO);
+
+    // Atualizar um cinema existente
+    @SecurityRequirement(name = "Bearer Auth")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @PutMapping("/{id}")
+    public CinemaResponseDTO atualizarCinema(@PathVariable Integer id, @RequestBody CinemaRequestDTO cinemaRequestDTO) {
+        return cinemaService.atualizarCinema(id, cinemaRequestDTO);
+    }
+
+    // Listar todos os cinemas
+    @SecurityRequirement(name = "Bearer Auth")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @GetMapping
+    public List<CinemaResponseDTO> listarCinemas() {
+        return cinemaService.listarCinemas();
+   
     }
 }
+    
