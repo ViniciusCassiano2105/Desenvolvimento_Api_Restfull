b61e6be90b9a71ec23e7a935d4ed39b091225a96
Vinicius Cassiano <115512244+ViniciusCassiano2105@users.noreply.github.com>
2024-10-26 21:19:31 -0300

    Ajustes - EnderecoRequestDTO EnderecoResponseDTO

--- projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/dto/EnderecoRequestDTO.java
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/dto/EnderecoRequestDTO.java
@@ -1,33 +1,86 @@
 package br.com.projeto.imperialflix.security.dto;
 
 public class EnderecoRequestDTO {
+    
+    private Integer id;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+    private String regiao;
 
-	private String complemento;
-	private Integer numero;
-	private String cep;
+    public Integer getId() {
+        return id;
+    }
 
-	public String getComplemento() {
-		return complemento;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
+    public String getCep() {
+        return cep;
+    }
 
-	public Integer getNumero() {
-		return numero;
-	}
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
+    public String getLogradouro() {
+        return logradouro;
+    }
 
-	public String getCep() {
-		return cep;
-	}
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
 
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
+    public String getComplemento() {
+        return complemento;
+    }
 
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(String regiao) {
+        this.regiao = regiao;
+    }
 }
--- projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/dto/EnderecoResponseDTO.java
+++ projetos_Individuais_Api/imperialflix/src/main/java/br/com/projeto/imperialflix/security/dto/EnderecoResponseDTO.java
@@ -1,25 +1,29 @@
 package br.com.projeto.imperialflix.security.dto;
 
-import br.com.projeto.imperialflix.security.entities.Endereco;
-
 public class EnderecoResponseDTO {
 
-	private String cep;
-	private String logradouro;
-	private String complemento;
-	private String bairro;
-	private String uf;
-	private String estado;
-	private String regiao;
-	private String localidade;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String regiao;
+    private String uf;
+    private String estado;
 
-	public String getLocalidade() {
-		return localidade;
-	}
+    public EnderecoResponseDTO(String cep, String logradouro, String complemento, String bairro, 
+                    String localidade, String regiao, String uf, String estado) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.regiao = regiao;
+        this.uf = uf;
+        this.estado = estado;
+    }
 
-	public void setLocalidade(String localidade) {
-		this.localidade = localidade;
-	}
+    public EnderecoResponseDTO() {}
 
 	public String getCep() {
 		return cep;
@@ -53,6 +57,22 @@ public class EnderecoResponseDTO {
 		this.bairro = bairro;
 	}
 
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+
+	public String getRegiao() {
+		return regiao;
+	}
+
+	public void setRegiao(String regiao) {
+		this.regiao = regiao;
+	}
+
 	public String getUf() {
 		return uf;
 	}
@@ -68,17 +88,5 @@ public class EnderecoResponseDTO {
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
-
-	public String getRegiao() {
-		return regiao;
-	}
-
-	public void setRegiao(String regiao) {
-		this.regiao = regiao;
-
-	}
-	
-	public Endereco toEndereco() {
-		return new Endereco(this.cep, this.logradouro, this.complemento, this.bairro, this.localidade, this.regiao, this.uf, this.estado);
-	}
+    
 }
